1cdc960988d803e636e6801b020276af5c207377
nitin-bnb <nitin.barai@bitnbinary.com>
2023-07-27 18:33:27 +0530

    form set

--- pdftoexcel.py
+++ pdftoexcel.py
@@ -1,9 +1,18 @@
 import json
+import time
+import datetime
+import boto3
 import PyPDF2
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.debug = True
+
+AWS_S3_BUCKET = 'pdftoexcel'
+AWS_S3_DOMAIN = 'https://ss-pdftoexcel.s3.ap-south-1.amazonaws.com'
+AWS_ACCESS_KEY_ID = 'AKIA3VHINVSYSOGK5OTY'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_REGION = 'ap-south-1'
 
 @app.route('/')
 def home():
@@ -13,6 +22,7 @@ def home():
 def pdftoexcel():
     if request.method == 'POST':
         file = request.form['file']
+        filename = request.form['filename']
         with open(file, "rb") as f:
             pdf_reader = PyPDF2.PdfReader(f)
             text_content = ""
@@ -20,10 +30,21 @@ def pdftoexcel():
                 lines = page.extract_text().split("\n")
                 for line in lines:
                     text_content += line + "\n"
-        return {
-            'statusCode': 200,
-            'body': json.dumps('File converted successfully.')
-        }
+            s3 = boto3.resource(
+                's3',
+                region_name=AWS_REGION,
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+            )
+            time_stamp = time.time()
+            current_date = datetime.datetime.now()
+            key = "{}{}/{}_{}, hope you're well!".format(current_date.year, current_date.month, str(time_stamp), filename)
+            s3.Object(AWS_S3_BUCKET, key).put(Body=text_content)
+            return {
+                'statusCode': 200,
+                'excel_file': '{}/{}'.format(AWS_S3_DOMAIN, key),
+                'body': json.dumps('File converted successfully.')
+            }
     else:
         return {
             'statusCode': 400,
