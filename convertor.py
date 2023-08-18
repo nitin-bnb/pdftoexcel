906cde55bafd8680fe977b49b4e2c9a6f232aa13
nitin-bnb <nitin.barai@bitnbinary.com>
2023-08-18 18:31:33 +0530

    set function for Lloyds Bank

--- convertor.py
+++ convertor.py
@@ -6,23 +6,25 @@ def convert(file, filename):
 
     data = tabula.read_pdf(file, stream=True, guess=True, pages='all', multiple_tables=True, pandas_options={'header': None})
     if data:
-        df = readandcleandata(data)
         # Add column names to the DataFrame
         if filename == "Natwest":
+            df = readandcleandata(data)
             df.columns = ['Date', 'Description', 'Type', 'Paid In', 'Paid Out', 'Ledger Balance']  
             processNetwest(df, filename)  
         elif filename == "LLoyds Bank":
             df.columns = ['Date', 'Activity', 'Paid Out', 'Paid In', 'Balance']
-            processLLoyds(df, filename)
+            processLLoyds(file, filename)
         elif filename == "LLoyds Bank 2":
+            df = readandcleandata(data)
             df.columns = ['Date', 'Description', 'Type', 'Paid In', 'Paid Out', 'Balance']
             processLLoyds2(df, filename)
         elif filename == "HSBC":
+            df = readandcleandata(data)
             df.columns = ['Date', 'Description', 'Paid In', 'Paid Out', 'Balance']
             processHSBC(df, filename)
         else:
+            df = readandcleandata(data)
             df = pd.concat(data, ignore_index=True)
             df.columns = ['Date', 'Description', 'Paid In', 'Paid Out', 'Balance']
-
-    #     
+  
     return ''
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import fitz
+import re
 import pandas as pd
 
 def remove_extra_headers(df):
@@ -54,32 +56,96 @@ def processNetwest(df, filename):
             worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
     return df
 
-def processLLoyds(df, filename):
-    date_pattern = r'^\d{2}/\w{3}/\d{2}$'
+def processLLoyds(file, filename):
+    words = ''
+    dates = []
+    descriptions = []
+    paid_out = []
+    paid_in = []
 
+    doc = fitz.open(file)
+    for page in doc:
+        words += page.get_text("Date", sort=True)
 
-    # Filter rows based on the date pattern
-    valid_date_rows = df['Date'].str.match(date_pattern, na=False)
-    df = df[valid_date_rows]
+    pattern = r'(\d{2}[A-Za-z]{3}\d{2})\s+([A-Za-z]{2,3})\s+(.+)'
+    pattern = pattern + r'|(\d{2}[A-Za-z]{3}\d{2})\s+RETURNED\s+'
+    matches = re.finditer(pattern, words)
+    match_positions = []
 
-    df = df.drop(columns=['Balance'])
+    for match in matches:
+        start = match.start()
+        end = match.end()
+        match_positions.append((start, end))
+
+    for num in range(len(match_positions)):
+        if num == len(match_positions) - 1:
+            data = words[match_positions[num][0]:match_positions[num][1]+20]
+            data = data.split('\n')
+            for i in range(len(data)):
+                data[i] = re.sub(r'\s+', '', data[i])
+            dates.append(data[0][:7])
+            if len(data)> 5 and not data[5] == '' and data[4] == '':
+                descriptions.append(data[0][7:])
+                paid_out.append(data[1])
+                paid_in.append(data[2])
+            elif data[1] == "RETURNEDDD":
+                descriptions.append(data[1])
+                paid_out.append(data[3])
+                paid_in.append(data[4])
+            elif '.' in data[1]:
+                descriptions.append(data[0][7:])
+                paid_out.append(data[1])
+                paid_in.append(data[2])
+            elif data[1]:
+                descriptions.append(data[1])
+                paid_out.append(data[2])
+                paid_in.append(data[3])
+            else:
+                descriptions.append(data[0][7:])
+                paid_out.append(data[2])
+                paid_in.append(data[3])
+        else:
+            data = words[match_positions[num][0]:match_positions[num + 1][0]]
+            data = data.split('\n')
+            for i in range(len(data)):
+                data[i] = re.sub(r'\s+', '', data[i])
+            dates.append(data[0][:7])
+            if len(data)> 5 and not data[5] == '' and data[4] == '':
+                descriptions.append(data[0][7:])
+                paid_out.append(data[1])
+                paid_in.append(data[2])
+            elif data[1] == "RETURNEDDD":
+                descriptions.append(data[1])
+                paid_out.append(data[3])
+                paid_in.append(data[4])
+            elif '.' in data[1]:
+                descriptions.append(data[0][7:])
+                paid_out.append(data[1])
+                paid_in.append(data[2])
+            elif data[1]:
+                descriptions.append(data[1])
+                paid_out.append(data[2])
+                paid_in.append(data[3])
+            else:
+                descriptions.append(data[0][7:])
+                paid_out.append(data[2])
+                paid_in.append(data[3])
+
+    df = pd.DataFrame({"Date": dates,"Description":descriptions, "Paid Out": paid_out, "Paid In": paid_in})
+
     with pd.ExcelWriter(f"{filename}.xlsx", engine="openpyxl") as writer:
-        df.to_excel(writer, sheet_name=f"{filename}", index=False)
+            df.to_excel(writer, sheet_name={filename}, index=False)
 
-        # Get the openpyxl workbook and worksheet objects
-        workbook = writer.book
-        worksheet = writer.sheets[f"{filename}"]
+            # Get the openpyxl workbook and worksheet objects
+            writer.book
+            worksheet = writer.sheets["LL"]
 
-        # Iterate through each column and set the optimal width
-        for column_cells in worksheet.columns:
-            length = max(len(str(cell.value)) for cell in column_cells)
-            worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
-    return df
+            # Iterate through each column and set the optimal width
+            for column_cells in worksheet.columns:
+                length = max(len(str(cell.value)) for cell in column_cells)
+                worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
 
 def processLLoyds2(df, filename):
-    # df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
-    # df = df.dropna(subset=['Date'])
-    # Regular expression to match the expected date format 'dd/mm/yyyy'
     date_pattern = r'^\d{2} \w{3} \d{2}$'
 
     # Filter rows based on the date pattern
@@ -92,7 +158,7 @@ def processLLoyds2(df, filename):
         df.to_excel(writer, sheet_name=f"{filename}", index=False)
 
         # Get the openpyxl workbook and worksheet objects
-        workbook = writer.book
+        writer.book
         worksheet = writer.sheets[f"{filename}"]
 
         # Iterate through each column and set the optimal width
@@ -114,7 +180,7 @@ def processHSBC(df, filename):
         df.to_excel(writer, sheet_name=f"{filename}", index=False)
 
         # Get the openpyxl workbook and worksheet objects
-        workbook = writer.book
+        writer.book
         worksheet = writer.sheets[f"{filename}"]
 
         # Iterate through each column and set the optimal width
