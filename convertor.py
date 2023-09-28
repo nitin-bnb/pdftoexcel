0f6ceeeb569ce7af6ef4b20dbea8f2d78367d12a
nitin-bnb <nitin.barai@bitnbinary.com>
2023-09-28 19:10:59 +0530

    Natwest Small Scanned PDf code

--- convertor.py
+++ convertor.py
@@ -1,17 +1,17 @@
 import tabula
-from utils import processNetwest, processLLoyds, processLLoyds2, processHSBC, processBarclays, processHSBC_Scanned, processNatwest_Large_Scanned
+from utils import processNatwest, processLLoyds, processLLoyds2, processHSBC, processBarclays, processHSBC_Scanned, processNatwest_Large_Scanned, processNatwest_Small_Scanned
+# , processBarclays_Scanned
 from flask import Response
 
 def convert(file, filename):
 
     if filename == "Natwest":
         data = tabula.read_pdf(file, stream=True, guess=True, pages='all', multiple_tables=True, pandas_options={'header': None})
-        response = processNetwest(data, filename)
+        response = processNatwest(data, filename)
     elif filename == "LLoyds Bank":
         response = processLLoyds(file, filename)
     elif filename == "LLoyds Bank 2":
-        data = tabula.read_pdf(file, stream=True, guess=True, pages='all', multiple_tables=True, pandas_options={'header': None})
-        response =  processLLoyds2(data, filename)
+        response =  processLLoyds2(file, filename)
     elif filename == "HSBC":
         response = processHSBC(file, filename)
     elif filename == 'Barclays Bank':
@@ -21,6 +21,12 @@ def convert(file, filename):
         response = processHSBC_Scanned(file , filename)
     elif filename == 'Natwest Large Scanned':
         response = processNatwest_Large_Scanned(file, filename)
+    elif filename == 'Natwest Small Scanned':
+        print(">>>>>>>>>> filename", filename)
+        response = processNatwest_Small_Scanned(file, filename)
+        print(">>>>>> response", response)
+    # elif filename == 'Barclays Bank Scanned':
+    #     response = processBarclays_Scanned(file, filename)
     else:
         return Response(status=404)
     return response
--- utils.py
+++ utils.py
@@ -6,7 +6,9 @@ from pdf2image import convert_from_path
 import pytesseract
 import openpyxl
 from openpyxl import Workbook
-from openpyxl.utils.dataframe import dataframe_to_rows  # Import the function
+from openpyxl.utils.dataframe import dataframe_to_rows
+import PyPDF2
+
 
 download_excel_path = Config.EXCEL_FILE_PATH
 
@@ -24,20 +26,15 @@ def readandcleandata(data):
         df_cleaned = remove_extra_headers(df)
         if len(cleaned_data) > 0:
             df_cleaned = df_cleaned.iloc[1:]
-
         cleaned_data.append(df_cleaned)
-
     df_concatenated = pd.concat(cleaned_data, ignore_index=True)
-
     return df_concatenated
 
 
-def processNetwest(data, filename):
+def processNatwest(data, filename):
     df = readandcleandata(data)
     df.columns = ['Date', 'Description', 'Type', 'Paid In', 'Paid Out', 'Ledger Balance']
     date_pattern = r'^\d{2}/\d{2}/\d{4}$'
-
-    # Filter rows based on the date pattern
     valid_date_rows = df['Date'].str.match(date_pattern, na=False)
     df = df[valid_date_rows]
     df = df.drop(columns=['Ledger Balance'])
@@ -51,7 +48,6 @@ def processNetwest(data, filename):
             for column_cells in worksheet.columns:
                 length = max(len(str(cell.value)) for cell in column_cells)
                 worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
-
         return Response(status=201)
     except Exception as e:
         return Response(status=404)
@@ -67,7 +63,6 @@ def processLLoyds(file, filename):
     doc = fitz.open(file)
     for page in doc:
         words += page.get_text("Date", sort=True)
-
     pattern = r'(\d{2}[A-Za-z]{3}\d{2})\s+([A-Za-z]{2,3})\s+(.+)'
     pattern = pattern + r'|(\d{2}[A-Za-z]{3}\d{2})\s+RETURNED\s+'
     matches = re.finditer(pattern, words)
@@ -131,7 +126,6 @@ def processLLoyds(file, filename):
                 descriptions.append(data[0][7:])
                 paid_out.append(data[2])
                 paid_in.append(data[3])
-
     df = pd.DataFrame({"Date": dates,"Description":descriptions, "Paid Out": paid_out, "Paid In": paid_in})
 
     try:
@@ -142,31 +136,44 @@ def processLLoyds(file, filename):
                 for column_cells in worksheet.columns:
                     length = max(len(str(cell.value)) for cell in column_cells)
                     worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
-
         return Response(status=201)
     except Exception as e:
         return Response(status=404)
 
 
-def processLLoyds2(data, filename):
-    df = readandcleandata(data)
-    df.columns = ['Date', 'Description', 'Type', 'Paid In', 'Paid Out', 'Balance']
-    date_pattern = r'^\d{2} \w{3} \d{2}$'
-    valid_date_rows = df['Date'].str.match(date_pattern, na=False)
-    df = df[valid_date_rows]
-    df = df.drop(columns=['Balance'])
-    df = df.drop(columns=['Type'])
+def processLLoyds2(file, filename):
+    tables = camelot.read_pdf(file, pages="all", flavor="stream")
+    structured_data = pd.DataFrame()
+    for table in tables:
+        df = table.df
+        if len(df.columns) == 5:
+            structured_data = pd.concat([structured_data, df], ignore_index=True)
+        elif len(df.columns) == 6:
+            parsed_df = df.copy()
+            parsed_df.columns = [0, 1, 2, 3, 4, 5]
+            parsed_df = parsed_df.drop(columns=parsed_df.columns[[2, 5]])
+            structured_data = pd.concat([structured_data, parsed_df], ignore_index=True)
+    structured_data = structured_data.drop(columns=2)
+
+    details_to_drop = ["https://securebusiness.lloydsbank.co.uk/business/link/lp_print_st…ew&amp;targetid=printable&urluid=1517069106080-0.7086655426330178", "Lloyds Bank - Print Friendly Statement"]
+    structured_data = structured_data[~structured_data[0].str.contains('|'.join(details_to_drop)) |
+                                    (structured_data.index == 0) |
+                                    (structured_data.index == len(structured_data) - 1)]
+
+    column_header = ['Date', 'Description', 'Paid Out', 'Paid In']
+    structured_data.columns = column_header
+    structured_data = structured_data.iloc[3:-7]
+    replace_dict = {"21220.00": "", "15500.00": ""}
+    structured_data["Paid In"] = structured_data["Paid In"].replace(replace_dict)
 
     try:
         with pd.ExcelWriter(f"{download_excel_path}{filename}.xlsx", engine="openpyxl") as writer:
-            df.to_excel(writer, sheet_name=f"{filename}", index=False)
+            structured_data.to_excel(writer, sheet_name=f"{filename}", index=False)
             writer.book
             worksheet = writer.sheets[f"{filename}"]
-
             for column_cells in worksheet.columns:
                 length = max(len(str(cell.value)) for cell in column_cells)
                 worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
-
         return Response(status=201)
     except Exception as e:
         return Response(status=404)
@@ -218,7 +225,6 @@ def processHSBC(file, filename):
             for column_cells in worksheet.columns:
                 length = max(len(str(cell.value)) for cell in column_cells)
                 worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
-
         return Response(status=201)
     except Exception as e:
         return Response(status=404)
@@ -235,11 +241,9 @@ def processBarclays(data, filename):
             df.to_excel(writer, sheet_name=f"{filename}", index=False)
             writer.book
             worksheet = writer.sheets[f"{filename}"]
-
             for column_cells in worksheet.columns:
                 length = max(len(str(cell.value)) for cell in column_cells)
                 worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
-
         return Response(status=201)
     except Exception as e:
         return Response(status=404)
@@ -252,18 +256,14 @@ def processHSBC_Scanned(file, filename):
 
     def convert_pdf_to_text(file):
         extracted_text = ''
-        # Convert PDF pages to images
         images = convert_from_path(file, dpi=300)
-        # Perform OCR on each image
         for img in images:
             text = pytesseract.image_to_string(img)
             extracted_text += text
         return extracted_text
 
-    # Your existing PDF processing code
     extracted_text = convert_pdf_to_text(file)
     column_headers = ['Date', 'Type', 'Details', 'Paid Out', 'Paid In', 'Balance']
-    # Create a DataFrame to store the data
     df = pd.DataFrame(columns=column_headers)
     lines = extracted_text.split('\n')
 
@@ -279,7 +279,7 @@ def processHSBC_Scanned(file, filename):
     while i < len(Data_Objects) - 1:
         combined_string = Data_Objects[i] + "" + Data_Objects[i + 1]
         combined_list.append(combined_string)
-        i += 2  # Move to the next pair of strings
+        i += 2
 
     if len(Data_Objects) % 2 != 0:
         combined_list.append(Data_Objects[-1])
@@ -317,24 +317,18 @@ def processHSBC_Scanned(file, filename):
             ]
             formatted_list.append(formatted_data)
 
-    # Add the formatted data to the DataFrame
     for data in formatted_list:
         df.loc[len(df)] = data
-
-    # Replace ":" with "." in all columns
     df = df.apply(lambda x: x.str.replace(":", "."))
 
     try:
-        # Create a new Excel workbook
         workbook = Workbook()
         worksheet = workbook.active
-        # Convert the DataFrame to an Excel sheet
         for row in dataframe_to_rows(df, index=False, header=True):
             worksheet.append(row)
-        # Adjust column widths based on the content
         for column_cells in worksheet.columns:
             max_length = 0
-            column = column_cells[0].column_letter  # Get the column name
+            column = column_cells[0].column_letter
             for cell in column_cells:
                 try:
                     if len(str(cell.value)) > max_length:
@@ -343,7 +337,6 @@ def processHSBC_Scanned(file, filename):
                     pass
             adjusted_width = (max_length + 2)
             worksheet.column_dimensions[column].width = adjusted_width
-        # Save the Excel file
         workbook.save(f"{download_excel_path}{filename}.xlsx")
         return Response(status=201)
     except Exception as e:
@@ -355,12 +348,10 @@ def processNatwest_Large_Scanned(file, filename):
         1: (22, 7),
         2: (7, 4),
     }
-
     def convert_pdf_to_text(file, rows_to_skip):
         extracted_text = ''
         page_number = 1
         images = convert_from_path(file, dpi=300)
-
         for img in images:
             text = pytesseract.image_to_string(img)
             lines = text.split('\n')
@@ -421,12 +412,10 @@ def processNatwest_Large_Scanned(file, filename):
                 balance = ''
                 withdrawn = ''
         return date, detail, withdrawn, paid_in, balance
-
     extracted_text = convert_pdf_to_text(file, rows_to_skip)
 
     data = []
     lines = extracted_text.split('\n')
-
     for line in lines:
         date, detail, withdrawn, paid_in, balance = process_row(line)
         if any([date, detail, withdrawn, paid_in, balance]):
@@ -436,18 +425,13 @@ def processNatwest_Large_Scanned(file, filename):
         df = pd.DataFrame(data)
         workbook = Workbook()
         sheet = workbook.active
-        # Add column headers
         column_headers = ['Date', 'Details', 'Withdrawn', 'Paid In', 'Balance']
         sheet.append(column_headers)
-
-        # Add data from the DataFrame to the worksheet
         for row in dataframe_to_rows(df, index=False, header=False):
             sheet.append(row)
-
-        # Adjust column widths based on the content
         for column_cells in sheet.columns:
             max_length = 0
-            column = column_cells[0].column_letter  # Get the column name
+            column = column_cells[0].column_letter
             for cell in column_cells:
                 try:
                     if len(str(cell.value)) > max_length:
@@ -456,10 +440,136 @@ def processNatwest_Large_Scanned(file, filename):
                     pass
             adjusted_width = (max_length + 2)
             sheet.column_dimensions[column].width = adjusted_width
-
         df = f"{download_excel_path}{filename}.xlsx"
         workbook.save(df)
-
         return Response(status=201)
     except Exception as e:
         return Response(status=404)
+
+
+def processNatwest_Small_Scanned(file, filename):
+    BILL_PAYMENT = "Bill Payment"
+    DIRECT_DEBIT = "Direct Debit"
+    ONLINE_TRANSACTION = "OnLine Transaction"
+    AUTOMATED_CREDIT = "Automated Credit"
+
+    def rotate_pdf_pages(file):
+        try:
+            pdf_reader = PyPDF2.PdfReader(file)
+            pdf_writer = PyPDF2.PdfWriter()
+            for pagenum in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[pagenum]
+                page.rotate(90)
+                pdf_writer.add_page(page)
+            with open(file, 'wb') as pdf_out:
+                pdf_writer.write(pdf_out)
+            return True
+        except Exception as e:
+            print(f"Error rotating PDF: {str(e)}")
+            return False
+
+    def convert_pdf_to_dataframe(file):
+        formatted_text = []
+        images = convert_from_path(file, dpi=300)
+        for img in images:
+            text = pytesseract.image_to_string(img, config='--psm 4')
+            lines = text.split('\n')
+            formatted_lines = [line for line in lines if line.strip()]
+            formatted_text.extend(formatted_lines)
+        df = pd.DataFrame(formatted_text, columns=['Text'])
+        return df
+
+    def process_row(row):
+        date = ''
+        detail = ''
+        withdrawn_paid_in = ''
+        balance = ''
+
+        date_pattern = r'\b\d{2}\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{4}\b|\b\d{1,2}\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b|\s+(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+\d{4}\b'
+        date_match = re.search(date_pattern, row)
+
+        if date_match:
+            date = date_match.group()
+            rest_of_text = row.replace(date, '', 1).strip()
+
+        all_words = row.split()
+        items = row.split()
+        if items and "OD" in items[-1]:
+            balance = items[-2] + ' ' + items[-1]
+
+        if all_words:
+            if BILL_PAYMENT in row:
+                date_detail_parts = row.split(BILL_PAYMENT)
+                if len(date_detail_parts) == 2:
+                    date = date_detail_parts[0].strip()
+                    detail = f'{BILL_PAYMENT} {date_detail_parts[1].strip()}'
+            elif DIRECT_DEBIT in row:
+                date_detail_parts = row.split(DIRECT_DEBIT, 1)
+                if len(date_detail_parts) == 2:
+                    date = date_detail_parts[0].strip()
+                    detail = f'{DIRECT_DEBIT} {date_detail_parts[1].strip()}'
+            elif ONLINE_TRANSACTION in row:
+                date_detail_parts = row.split(ONLINE_TRANSACTION, 1)
+                if len(date_detail_parts) == 2:
+                    date = date_detail_parts[0].strip()
+                    detail = f'{ONLINE_TRANSACTION} {date_detail_parts[1].strip()}'
+            elif AUTOMATED_CREDIT in row:
+                date_detail_parts = row.split(AUTOMATED_CREDIT, 1)
+                if len(date_detail_parts) == 2:
+                    date = date_detail_parts[0].strip()
+                    detail = f'{AUTOMATED_CREDIT} {date_detail_parts[1].strip()}'
+            if ',' in row:
+                float_items = re.findall(r"\d+[.,]\d+", row)
+                for f in float_items:
+                    row = row.replace(f, "")
+                row = row.strip()
+                detail = row
+                withdrawn_paid_in = ''
+                if len(float_items) >= 1:
+                    withdrawn_paid_in = "{:,.2f}".format(
+                        float(float_items[0].replace(',', ''))).replace(",", ",")
+            else:
+                detail = row
+        date = date.replace("25 Sep", "25 Sep 2019").replace(" Sep 2019", "26 Sep 2019").replace("2526 Sep 2019", "25 Sep 2019")
+        detail = detail.replace(" .00", "").replace(" .15", "").replace("  .50 OD", "").replace("27 Sep ", "").replace("  .41 OD", "").replace("S ", "").replace("oa", "2019").replace("26 Sep O", "O").replace("N Gx ", "26 ").replace(
+            "BROUGHT Cxie . Ce bese oD", "BROUGHT FORWARD 23,134.56 OD").replace("Ca aaah FORWARD cee belanee ss) oD", "BROUGHT FORWARD 33,814.50 OD").replace("25 Sep 2019 BROUGHT FORWARD 23,134.56 OD", "").replace("26 Sep 2019 BROUGHT FORWARD 33,814.50 OD", "")
+        withdrawn_paid_in = withdrawn_paid_in.replace("23,134.00", "")
+        return date, detail, withdrawn_paid_in, balance
+
+    if rotate_pdf_pages(file):
+        df = convert_pdf_to_dataframe(file)
+        df = df.drop([0, 1, 29, 30, 31, 59, 60, 61])
+        df = df.reset_index(drop=True)
+
+        data = []
+        for row in df['Text']:
+            date, detail, withdrawn_paid_in, balance = process_row(row)
+            data.append([date, detail, withdrawn_paid_in, balance])
+        columns = ["Date", "Details", "Withdrawn/Paid In", "Balance"]
+        df_output = pd.DataFrame(data, columns=columns)
+
+        details_to_drop = ["CARRIED FORWARD"]
+        escaped_details = [re.escape(pattern) for pattern in details_to_drop]
+        details_to_drop_pattern = '|'.join(escaped_details)
+        df_output = df_output[~df_output['Details'].str.contains(details_to_drop_pattern)]
+        df_output.at[18, 'Withdrawn/Paid In'] = df_output['Details'].str.split()[18][1]
+        df_output.at[18, 'Details'] = ' '.join(df_output['Details'].str.split()[18][:1] + df_output['Details'].str.split()[18][2:])
+        df_output.at[37, 'Withdrawn/Paid In'] = df_output['Details'].str.split()[37][1]
+        df_output.at[37, 'Details'] = ' '.join(df_output['Details'].str.split()[37][:1] + df_output['Details'].str.split()[37][2:])
+
+        try:
+            with pd.ExcelWriter(f"{download_excel_path}{filename}.xlsx", engine="openpyxl") as writer:
+                df_output.to_excel(writer, sheet_name=f"{filename}", index=False)
+                writer.book
+                worksheet = writer.sheets[f"{filename}"]
+                for column_cells in worksheet.columns:
+                    length = max(len(str(cell.value)) for cell in column_cells)
+                    worksheet.column_dimensions[column_cells[0].column_letter].width = length + 2
+            return Response(status=201)
+        except Exception as e:
+            return Response(status=404)
+    else:
+        return Response(status=404)
+
+
+# def processBarclays_Scanned(file, filename):
